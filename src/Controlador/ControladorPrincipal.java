7ddac21cc6a1aa3e6eb47d45a8cdd9641b1d82fa
Nicolas O. <nico.olivaresg@hotmail.cl>
2016-06-23 19:21:34 -0400

    Creación de proyecto, inclusión de clases de vista, controlador y modelo

---
+++ src/Controlador/ControladorPrincipal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.VistaPrincipal;
+/**
+ *
+ * @author nicoo
+ */
+public class ControladorPrincipal {
+    private VistaPrincipal vP;
+    private ControladorAdministracionTalleres cAdminTalleres;
+    private ControladorAgendamientoHoras cAgendHoras;
+    private ControladorAutenticacion cAuten;
+    private ControladorCuenta cCuenta;
+    private ControladorMantenimiento cManten;
+    private ControladorRegistro cReg;
+    private ControladorTest cTest;
+    public ControladorPrincipal(){
+        vP = new VistaPrincipal(this);
+    }
+}
